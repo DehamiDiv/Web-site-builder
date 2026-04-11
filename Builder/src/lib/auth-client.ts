ad0a917bce8252903e59e6bfe0abf22961cbbffc
DehamiDiv <dehamidivyanjali166@gmail.com>
2026-04-11 06:37:06 +0530

    fix(frontend): implement relative URLs for production to fix CORS fetch errors

--- Builder/src/lib/auth-client.ts
+++ Builder/src/lib/auth-client.ts
@@ -1,7 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    
-    baseURL: import.meta.env.VITE_BASEURL,
+    baseURL: import.meta.env.PROD ? import.meta.env.VITE_BASEURL || '/api/auth' : 'http://localhost:3000/api/auth',
     fetchOptions:{
         credentials:"include"
     }
